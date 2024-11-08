61adc362e486974471710d19f1454e807778c6d8
cjiang-git <cj2630@columbia.edu>
2024-11-07 20:26:20 -0800

    update embeddings fixed and completed

--- download_files.sh
+++ download_files.sh
@@ -10,6 +10,10 @@ STITCHDB_PATH="http://stitch.embl.de/download/protein_chemical.links.transfer.v5
 STRINGDB_SEQ_PATH="https://stringdb-downloads.org/download/protein.sequences.v12.0.fa.gz"
 STITCHDB_SMILE_PATH="http://stitch.embl.de/download/chemicals.v5.0.tsv.gz"
 
+STRINGDB_HU_PATH="https://stringdb-downloads.org/download/protein.physical.links.full.v12.0/9606.protein.physical.links.full.v12.0.txt.gz"
+STRINGDB_HU_SEQ_PATH='https://stringdb-downloads.org/download/protein.sequences.v12.0/9606.protein.sequences.v12.0.fa.gz'
+
+: '
 # STRING DB
 echo "Downloading STRING DB"
 wget -nc $STRINGDB_PATH -O "${STORAGE_PATH}stringdb.txt.gz"
@@ -18,7 +22,18 @@ gzip -dk "${STORAGE_PATH}stringdb.txt.gz"
 echo "Downloading STRING DB SEQUENCES"
 wget -nc $STRINGDB_SEQ_PATH -O "${STORAGE_PATH}stringdb_seq.fa.gz"
 gzip -dk "${STORAGE_PATH}stringdb_seq.fa.gz"
+'
 
+# STRING DB Human
+echo "Downloading STRING DB"
+wget -nc $STRINGDB_HU_PATH -O "${STORAGE_PATH}stringdb_human.txt.gz"
+gzip -dk "${STORAGE_PATH}stringdb_human.txt.gz"
+
+echo "Downloading STRING DB SEQUENCES"
+wget -nc $STRINGDB_HU_SEQ_PATH -O "${STORAGE_PATH}stringdb_seq_human.fa.gz"
+gzip -dk "${STORAGE_PATH}stringdb_seq_human.fa.gz"
+
+: '
 # STITCH DB
 #if [ ! -f "${STORAGE_PATH}stitchdb" ]; then
 echo "Downloading STITCH DB"
@@ -28,7 +43,5 @@ gzip -dk "${STORAGE_PATH}stitchdb.txt.gz"
 echo "Downloading STITCH DB SMILES"
 wget -nc $STITCHDB_SMILE_PATH -O "${STORAGE_PATH}stitchdb_smile.tsv.gz"
 gzip -dk "${STORAGE_PATH}stitchdb_smile.tsv.gz"
-
-
-
+'
 
